5fdb1f9f36746a6e68350a6212b856cd4647cc40
Ayan Alam <infosys316@gmail.com>
2025-04-26 18:58:19 +0530

    updated framer-motion

--- src/Components/Home/HomeEarn.jsx
+++ src/Components/Home/HomeEarn.jsx
@@ -61,6 +61,7 @@ const HomeEarn = () => {
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
+      viewport={{ once: true, amount: 0.3 }} // <- important
     >
       <div className="container-fluid" style={{ backgroundColor: "#161827" }}>
         <div className="container">
