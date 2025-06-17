1a29616f2da12e9e17ad9b7ccc41b336b7e96b0e
Ayan Alam <infosys316@gmail.com>
2025-06-17 19:43:01 +0530

    Landing page improved

--- src/Components/LandingPage/LandingNav.jsx
+++ src/Components/LandingPage/LandingNav.jsx
@@ -5,14 +5,22 @@ const LandingNav = () => {
   return (
     <div className={`container-fluid p-0 ${style.landingNav}`}>
       <div>
-        <nav className="d-flex">
-          <div className={`container ${style.landingNavBlur}`}>
+        <nav>
+          <div
+            className={`container d-flex align-items-center ${style.landingNavBlur}`}
+          >
             <div className={style.VeLoopLogo}>
               <img
                 src={VeLoopLogoAni}
                 alt="Watch Ads & Earn Real Cash Online – Only on VELoop"
               />
             </div>
+            <h2 className={style.landingLogoTxt}>
+              <strong>
+                <span style={{ color: "#ffd700", marginRight: "3px" }}>VE</span>
+                Loop
+              </strong>
+            </h2>
           </div>
         </nav>
       </div>
