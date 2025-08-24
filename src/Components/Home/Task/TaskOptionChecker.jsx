d69e0f5e2bc00e327851c41f348ba70e8d11c53b
Ayan Alam <infosys316@gmail.com>
2025-08-24 23:22:12 +0530

    Option checker on construction

--- src/Components/Home/Task/TaskOptionChecker.jsx
+++ src/Components/Home/Task/TaskOptionChecker.jsx
@@ -1,4 +1,6 @@
 import style from "./TaskOptionChecker.module.css";
+import capWrong from "../../../assets/captcha-opt/wrong-opt.png";
+import capCorrect from "../../../assets/captcha-opt/correct-opt.png";
 import { useLocation } from "react-router-dom";
 import CommonNavArr from "../../CommonComponents/CommonNavArr";
 
@@ -6,7 +8,7 @@ const TaskOptionChecker = () => {
   const location = useLocation();
   const { result, resultBool } = location.state || {};
 
-  console.log(resultBool);
+  console.log(resultBool); // returns True or False
 
   return (
     <>
@@ -15,6 +17,27 @@ const TaskOptionChecker = () => {
         name={result}
         navigation={"/captcha-tasks"}
       ></CommonNavArr>
+
+      <div className="container-fluid">
+        <div className="container">
+          <div className="d-flex flex-column justify-content-center align-items-center">
+            <div className={style.optImgCont}>
+              <img
+                className={style.optImg}
+                src={resultBool ? capCorrect : capWrong}
+                alt=""
+              />
+            </div>
+            <p
+              className={`${style.optPara} ${
+                resultBool ? style.optCorrect : style.optWrong
+              }`}
+            >
+              {result}
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
