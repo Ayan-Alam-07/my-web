fabedd92da63a7a27b92839e6cf815112bd2c2cf
Ayan-Alam-07 <infosys3161@gmail.com>
2026-07-13 20:01:02 +0530

    enhancing the UI & spacing

--- src/Components/Staking/page/StakingPage.jsx
+++ src/Components/Staking/page/StakingPage.jsx
@@ -1,12 +1,13 @@
 import StakingHero from "../components/StakingHero/StakingHero";
 import CommonNavArr from "../../CommonComponents/CommonNavArr";
+import styles from "./StakingPage.module.css";
 
 const StakingPage = () => {
   return (
-    <>
+    <main className={styles.main}>
       <CommonNavArr id={25} navigation={"/home"} />
       <StakingHero />
-    </>
+    </main>
   );
 };
 export default StakingPage;
